f4a2d0bd18df7365f39e05d17df5a4f6d5dabf67
lay <kmr345@naver.com>
2025-03-03 09:17:05 +0900

    init

---
+++ src/main/java/com/pluxity/troubleshooting/TroubleShootingApplication.java
@@ -0,0 +1,13 @@
+package com.pluxity.troubleshooting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroubleShootingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TroubleShootingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pluxity/troubleshooting/TroubleShootingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluxity.troubleshooting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroubleShootingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
